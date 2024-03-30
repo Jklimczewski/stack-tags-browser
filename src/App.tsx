7935ef0dae7a6feee83efdfb82d66637459fbb7e
Jakub Klimczewski <kukli8@o2.pl>
2024-03-30 23:19:59 +0100

    Created query provider and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Routes } from "react-router-dom";
-import { Loading } from "./components/Loading";
+import { Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
+import { Loading } from "./components/Loading";
+import { Navbar } from "./components/Navbar";
+import PageNotFound from "./pages/PageNotFound";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Suspense fallback={<Loading />}>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
       </Suspense>
     </>
   );
---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren, useState } from "react";
+
+function QueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 30000,
+            refetchOnWindowFocus: false,
+            retry: 2,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export { QueryProvider };
